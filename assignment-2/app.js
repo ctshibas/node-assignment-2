87742fec3eac6af1bc6e1fec732219b33864ffb1
Corneille Tshibasu <corneilletshibasu@gmail.com>
2020-11-10 16:55:37 +0000

    Update app.js

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -13,14 +13,26 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 // second step: create 2 middleware functions that will log
-app.use((req, res, next) => {
-    console.log("I'm in this first middleware");
-    //next();
+// app.use((req, res, next) => {
+//     console.log("I'm in this first middleware");
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log("I'm in this second middleware and check your page for a response");
+//     res.send('<h1>Welcome to the dummy page</h1>');
+// });
+
+// third step: create 2 middleware functions with filters on them, both handling req + res
+app.use('/users', (req, res, next) => {
+    console.log('I\'m in this other middleware and will return something else');
+    res.send('<h1>Welcome to the users page!</h1>');
 });
 
-app.use((req, res, next) => {
-    console.log("I'm in this second middleware and check your page for a response");
-    res.send('<h1>Welcome to the dummy page</h1>');
+app.use('/', (req, res, next) => {
+    console.log('I\'m in this function and will return something');
+    res.send('<h1>Welcome to the home page!</h1>');
 });
 
+// listening in on the PORT 3000
 app.listen(PORT);
